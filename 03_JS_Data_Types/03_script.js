360ae25482f3db4f2b54ff7722b5a28a829223ad
vaibhav kalne <vaibhavkalne143@gmail.com>
2018-10-07 15:50:12 +0530

    added

---
+++ 03_JS_Data_Types/03_script.js
@@ -0,0 +1,35 @@
+//undefined example
+var abc;
+console.log("value "+abc+ " Data Type "+ typeof abc);
+// number example
+var jsversion = 5;
+console.log("value "+jsversion+ " data type "+typeof jsversion);
+
+// String Examples
+var empName = "vaibhav";
+console.log("value " +empName+ " data type "+typeof empName);
+
+// boolean Examples
+var isJSEasy = true;
+console.log("value " +isJSEasy+ " data type "+typeof isJSEasy);
+
+//Null example
+var test = null;
+console.log("value " +test+ " data type "+typeof test);
+
+// reassignment example of variables
+
+var someVar;
+console.log("value of somevar is: "+someVar);
+
+var someVar = 10;
+console.log("value of somevar is: "+someVar);
+
+var someVar = "test";
+console.log("value of somevar is: "+someVar);
+
+var someVar =true;
+console.log("value of somevar is: "+someVar);
+
+var someVar = null;
+console.log("value of somevar is: "+someVar);
